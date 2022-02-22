ba97c29379d73575ddb3c4eba37048a8baea2187
hinal.patel <hinal.patel@1R-79>
2022-02-22 16:02:33 +0530

    Add project files.

---
+++ EmployeeAdapter.cs
@@ -0,0 +1,62 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.RecyclerView.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Recycler
+{ 
+    class EmployeeAdapter : RecyclerView.Adapter
+    {
+        private List<string> _employeesName;
+        private event EventHandler<int> ItemClick;
+        public EmployeeAdapter(List<string> employeesName)
+        {
+            _employeesName = employeesName;
+        }
+        public override int ItemCount => _employeesName.Count;
+        
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            EmployeeViewHolder viewHolder = holder as EmployeeViewHolder;
+            viewHolder.BindData(_employeesName[position]);
+        }
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View view = LayoutInflater.From(parent.Context).
+            Inflate(Resource.Layout.employee_list_row_item, parent, attachToRoot: false);
+            return new EmployeeViewHolder(view,OnClick);
+        }
+
+        private void OnClick(int position)
+        {
+            if (ItemClick != null)
+            {
+                ItemClick?.Invoke(this, position);
+            }
+            ItemClick?.Invoke(this, position);
+        }
+        class EmployeeViewHolder : RecyclerView.ViewHolder
+        {
+            private TextView _employeeNameTextView;
+            
+            public EmployeeViewHolder(View itemView, Action<int> listener) : base(itemView)
+            {
+                var textView = itemView.FindViewById<TextView>(Resource.Id._employeeNameTextView);
+                _employeeNameTextView = textView;
+            _employeeNameTextView.Click += (s, e) => listener.Invoke(LayoutPosition);
+            }
+            internal void BindData(string employeeName)
+            {
+                _employeeNameTextView.Text = employeeName;
+            }
+        }
+    }
+}
+    
---
+++ MainActivity.cs
@@ -0,0 +1,94 @@
+﻿
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using AndroidX.RecyclerView.Widget;
+using Recycler;
+using System;
+using System.Collections.Generic;
+
+namespace Recycler
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme" , MainLauncher =true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private RecyclerView _emploeeRecyclerView;
+        private List<string> _employeeNames;
+        private EmployeeAdapter _employeeAdapter;
+        private ListView _employeeList;
+        RecyclerView.LayoutManager _layoutManager;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            _emploeeRecyclerView = FindViewById<RecyclerView>(Resource.Id._employeeRecyclerView);
+            _employeeList = FindViewById<ListView>(Resource.Id._employeeList);
+
+            GenerateDummyRecords();
+            _layoutManager = new GridLayoutManager(this,2,GridLayoutManager.Vertical,false);
+            _employeeAdapter = new EmployeeAdapter(_employeeNames);
+            _emploeeRecyclerView.SetLayoutManager(_layoutManager);
+            _emploeeRecyclerView.SetAdapter(_employeeAdapter);
+
+           
+            
+        }
+
+        private void _employeeAdapter_ItemClick(object sender, int e)
+        {
+            //Toast.MakeText(this, "hii" + _employeeNames[].ToString(), ToastLength.Long).Show();
+            Toast.MakeText(this,"Hoo"+ _employeeNames[e].ToString(), ToastLength.Short).Show();
+        }
+
+        private void GenerateDummyRecords()
+        {
+            _employeeNames = new List<string>();
+
+            _employeeNames.Add(item: "Car");
+            _employeeNames.Add(item: "Bag");
+            _employeeNames.Add(item: "Bottol");
+            _employeeNames.Add(item: "pencil");
+            _employeeNames.Add(item: "eraser");
+            _employeeNames.Add(item: "Bag");
+            _employeeNames.Add(item: "Bottol");
+            _employeeNames.Add(item: "eraser");
+            _employeeNames.Add(item: "pencil");
+            _employeeNames.Add(item: "Car");
+            _employeeNames.Add(item: "Bag");
+            _employeeNames.Add(item: "Bottol");
+            _employeeNames.Add(item: "pencil");
+            _employeeNames.Add(item: "eraser");
+            _employeeNames.Add(item: "Car");
+            _employeeNames.Add(item: "Bag");
+            _employeeNames.Add(item: "Bottol");
+            _employeeNames.Add(item: "pencil");
+            _employeeNames.Add(item: "eraser");
+            _employeeNames.Add(item: "Car");
+            _employeeNames.Add(item: "Bag");
+            _employeeNames.Add(item: "Car");
+            _employeeNames.Add(item: "Bag");
+            _employeeNames.Add(item: "Bottol");
+            _employeeNames.Add(item: "pencil");
+            _employeeNames.Add(item: "eraser");
+            _employeeNames.Add(item: "Bag");
+            _employeeNames.Add(item: "Bottol");
+            _employeeNames.Add(item: "eraser");
+            _employeeNames.Add(item: "pencil");
+            _employeeNames.Add(item: "Car");
+            _employeeNames.Add(item: "Bag");
+            _employeeNames.Add(item: "Bottol");
+            _employeeNames.Add(item: "pencil");
+            _employeeNames.Add(item: "eraser");
+            _employeeNames.Add(item: "Car");
+            _employeeNames.Add(item: "Bag");
+            _employeeNames.Add(item: "Bottol");
+            _employeeNames.Add(item: "pencil");
+            _employeeNames.Add(item: "eraser");
+            _employeeNames.Add(item: "Car");
+            _employeeNames.Add(item: "Bag");
+        }
+}
+}
